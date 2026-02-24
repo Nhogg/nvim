8178e90e6a33c9530680f731574491c0047305c5
Nathan Hogg <nhogg@login.cl.luc.edu>
2026-02-24 14:22:59 -0600

    Fix treesitter errors

--- lua/mhyatt000/plugins/init.lua
+++ lua/mhyatt000/plugins/init.lua
@@ -50,8 +50,41 @@ return {
             vim.cmd([[colorscheme gruvbox]])
         end
     },
+{
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        config = function()
+            -- We put this inside a pcall (protected call) so if treesitter
+            -- isn't installed yet, it won't crash your whole editor!
+            local status_ok, configs = pcall(require, "nvim-treesitter.configs")
+            if not status_ok then
+                return
+            end
 
-    'nvim-treesitter/nvim-treesitter',
+            configs.setup({
+                -- A list of parser names you want installed by default
+                ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "python", "bash" },
+
+                -- Install parsers synchronously (only applied to `ensure_installed`)
+                sync_install = false,
+
+                -- Automatically install missing parsers when entering buffer
+                auto_install = true,
+
+                highlight = {
+                    enable = true,
+                    -- Disable for giant files to prevent freezing
+                    disable = function(lang, buf)
+                        local max_filesize = 100 * 1024 -- 100 KB
+                        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+                        if ok and stats and stats.size > max_filesize then
+                            return true
+                        end
+                    end,
+                },
+            })
+        end,
+    },
 
     {
         'VonHeikemen/lsp-zero.nvim',
