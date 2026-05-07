46726dd9b0f84b0cde4d5b21a3b6ccb90828dfbc
nhogg <nathanhogg1223@gmail.com>
2026-05-07 17:18:34 -0500

    add tab complete

--- lua/nhogg/plugins/init.lua
+++ lua/nhogg/plugins/init.lua
@@ -115,12 +115,17 @@ return {
     {
         'hrsh7th/nvim-cmp',
         config = function()
+            local cmp = require 'cmp'
             require 'cmp'.setup {
                 snippet = {
                     expand = function(args)
                         require 'luasnip'.lsp_expand(args.body)
                     end
                 },
+                mapping = cmp.mapping.preset.insert({
+                    ['<Tab>'] = cmp.mapping.confirm({ select = true }),
+                    ['<C-Space>'] = cmp.mapping.complete(),
+                }),
                 sources = {
                     { name = 'luasnip', option = { use_show_condition = false } },
                 },
